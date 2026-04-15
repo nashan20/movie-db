10b8995f873fa541c9132949f2ec9533f12604c8
MINGW64 <mnashan20@gmail.com>
2026-04-15 11:15:53 +0530

    Implemented saveFavourites using localStorage

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const API_KEY = "397bfad5";
 function getFavourites() {
   return JSON.parse(localStorage.getItem("favourites")) || [];
 }
+
+function saveFavourites(favs) {
+  localStorage.setItem("favourites", JSON.stringify(favs));
+}
